aa8a280edecefbf3e1c452895accf26518e47ec6
jkinsfat <jkinsfat@w.edu>
2017-05-30 11:59:21 -0700

    Tested addClass, SlideToggle, Append, Remove, Hover, Show and Hide

--- JS/jPlayJS.js
+++ JS/jPlayJS.js
@@ -1,5 +1,23 @@
 $("Document").ready(function() {
-
-
-
+    var triggered = true;
+    $(".toggleClassOn").click(function() {
+        $('p:first-of-type').addClass('bigRed');
+    })
+    $(".toggleClassOff").click(function() {
+        $('p:first-of-type').removeClass('bigRed');
+    })
+    $(".toggleSlide").click(function() {
+        triggered = !triggered;
+        $('.catBread').slideToggle();
+        if (!triggered) {
+            $('div:nth-of-type(2)').append("<h2> I AM THE PAW </h2>");
+        } else {
+            $('div:nth-of-type(2) h2').remove();
+        }
+    })
+    $(".clickme").hover(function() {
+        $('.nyahnyah').show();
+    }, function() {
+        $('.nyahnyah').hide();
+    })
 });
